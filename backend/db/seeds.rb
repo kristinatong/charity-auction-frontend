614d64192f9a7480ac979c4a9777a7cde14cd81d
mccoleman75225 <michaelcoleman19@gmail.com>
2018-10-19 12:17:51 -0400

    attempted fix

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -6,12 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 #
-# User.create({name: "kristina", email:"kt@aol.com", password:"123", prof_pic:"tbd"})
-# User.create({name: "kristina", email:"kt@aol.com", password:"123", prof_pic:"tbd"})
-# User.create({name: "kristina", email:"kt@aol.com", password:"123", prof_pic:"tbd"})
-# User.create({name: "kristina", email:"kt@aol.com", password:"123", prof_pic:"tbd"})
-# User.create({name: "kristina", email:"kt@aol.com", password:"123", prof_pic:"tbd"})
+User.create({name: "kristina", email:"adsfasdfasdf@aol.com", password:"password", prof_pic:"tbd"})
+User.create({name: "Michael", email:"justin1@aol.com", password:"password", prof_pic:"tbd"})
+User.create({name: "Andrew", email:"justin2@aol.com", password:"password", prof_pic:"tbd"})
+User.create({name: "Jesse", email:"justin3@aol.com", password:"password", prof_pic:"tbd"})
+User.create({name: "Olivia", email:"justin4@aol.com", password:"password", prof_pic:"tbd"})
 
-# Auction.create({item_name:"whatever",item_description:"desc",item_pic:"tbd", seller_id:1})
+Auction.create({item_name:"whatever",item_description:"desc",item_pic:"tbd", seller_id:1})
+Auction.create({item_name:"item",item_description:"desc",item_pic:"tbd", seller_id:1})
+Auction.create({item_name:"flute",item_description:"desc",item_pic:"tbd", seller_id:1})
+Auction.create({item_name:"tuba",item_description:"desc",item_pic:"tbd", seller_id:1})
+Auction.create({item_name:"trumpet",item_description:"desc",item_pic:"tbd", seller_id:1})
+
+
 
 Bid.create({amount:10,bidder_id:2,auction_id:1})
+Bid.create({amount:10,bidder_id:1,auction_id:2})
+Bid.create({amount:10,bidder_id:3,auction_id:4})
+Bid.create({amount:10,bidder_id:5,auction_id:2})
+Bid.create({amount:10,bidder_id:4,auction_id:3})
